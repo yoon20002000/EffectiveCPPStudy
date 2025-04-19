96081e9d2664d33b18fbe2ecf8c03a60f11a62ff
yoon20002000 <yoon20002000@naver.com>
2025-04-19 11:36:41 +0900

    06,08

---
+++ 06. 컴파일러가 만들어낸 함수가 필요없으면 확실히 이들의 사용을 금해라/UncopyableEx.h
@@ -0,0 +1,25 @@
+﻿// C++ 98/03
+class UncopyableEx
+{
+protected:              // 파생된 객체에 대해 생성과 소멸을 허용함.
+    UncopyableEx(){}
+    ~UncopyableEx(){}
+private:
+    UncopyableEx(const UncopyableEx&);          // 하지만 복사는 방지
+    UncopyableEx& operator=(const UncopyableEx&);
+}
+
+
+class HomeForSale : private UncopyableEx // 복사생성자, 복사대입자는 선언 불가
+{
+}
+
+// C++ 11 이상
+class UncopyableEx
+{
+protected:              // 파생된 객체에 대해 생성과 소멸을 허용함.
+    UncopyableEx(){}
+    ~UncopyableEx(){}
+    UncopyableEx(const UncopyableEx&) = delete;          // 하지만 복사는 방지
+    UncopyableEx& operator=(const UncopyableEx&) = delete;
+}
